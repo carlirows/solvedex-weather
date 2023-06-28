2c3a89b623c77b78106f4ffb2a3daf9fc5498187
carlirows <carlirows@gmail.com>
2023-06-28 14:00:42 -0500

    images erased to be replaced

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -9,13 +9,15 @@ export const WeatherCard = ({weather, units}) => {
         : (
             <div className='weather-card'>
                 <img src={`./src/assets/images/i${weather.icon}.png`} alt="Image" className="weather-icon"/>
-                <h2>{weather.city} <span className='weather-text'>{Math.floor(parseFloat(weather.temp))} {units === 'metric' ? '°C' : '°F'}</span></h2>
-                <p>Feels Like: <span className='weather-text'>{Math.floor(parseFloat(weather.feeling))} {units === 'metric' ? '°C' : '°F'}</span> </p>
-                <p>Max Temp: <span className='weather-text'>{Math.floor(parseFloat(weather.maxTemp))} {units === 'metric' ? '°C' : '°F'}</span> </p>
-                <p>Min Temp: <span className='weather-text'>{Math.floor(parseFloat(weather.minTemp))} {units === 'metric' ? '°C' : '°F'}</span> </p>
-                <p>Wind: <span className='weather-text'>{Math.floor(parseFloat(weather.wind))} {units === 'metric' ? 'm/s' : 'mph'}</span> </p>
-                <p>{weather.day}: <span className='weather-text'>{weather.time}</span> </p>
-                <p>{weather.desc}</p>
+                <div className="weather-info">
+                  <h2>{weather.city} <span className='weather-text'>{Math.floor(parseFloat(weather.temp))} {units === 'metric' ? '°C' : '°F'}</span></h2>
+                  <p>Feels Like: <span className='weather-text'>{Math.floor(parseFloat(weather.feeling))} {units === 'metric' ? '°C' : '°F'}</span> </p>
+                  <p>Max Temp: <span className='weather-text'>{Math.floor(parseFloat(weather.maxTemp))} {units === 'metric' ? '°C' : '°F'}</span> </p>
+                  <p>Min Temp: <span className='weather-text'>{Math.floor(parseFloat(weather.minTemp))} {units === 'metric' ? '°C' : '°F'}</span> </p>
+                  <p>Wind: <span className='weather-text'>{Math.floor(parseFloat(weather.wind))} {units === 'metric' ? 'm/s' : 'mph'}</span> </p>
+                  <p>{weather.day}: <span className='weather-text'>{weather.time}</span> </p>
+                  <p>{weather.desc}</p>
+                </div>
             </div>
         ) 
             
